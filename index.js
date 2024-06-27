6885322350ee50dd34b2dd95a461dd042be4d38d
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-27 16:41:58 +0200

    set connection to mongoDB

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', async (req, res) => {
-  res.send('Hello from DALL-E');
+  res.send('Hello from DALL-EE');
 });
 
 const startServer = async () => {
